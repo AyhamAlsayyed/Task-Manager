19578f71fd150d299668c3d82098ddf6f913c1ec
Ayham Alsayyed <ayham.sayyed.04@gmail.com>
2025-10-15 03:14:48 +0300

    Add a rest password via email

--- app/views/home_view.py
+++ app/views/home_view.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    notifications = request.user.notifications.filter(is_read=False).order_by("-created_at")
+    notifications = None
+    if request.user.is_authenticated:
+        notifications = request.user.notifications.filter(is_read=False).order_by("-created_at")
     return render(request, "app/home.html", {"notifications": notifications})
